b442770fb58afea99303c47b1fd016171234baed
Van Brigoli <vanbrixx@gmail.com>
2020-05-18 11:26:47 +0800

    add environment vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { UsersModule } from './resources/users/users.module';
 import { AuthModule } from './resources/auth/auth.module';
@@ -10,11 +11,15 @@ import { SubscriptionsModule } from './resources/subscriptions/subscriptions.mod
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/elearningv2', {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useFindAndModify: false,
-    }),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+      },
+    ),
     AuthModule,
     UsersModule,
     SubjectsModule,

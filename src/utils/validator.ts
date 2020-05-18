986a4b960599f28b46000f59c3cd3c397db1d5da
Van Brigoli <vanbrixx@gmail.com>
2020-05-18 15:05:30 +0800

    add security

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,7 +1,7 @@
-import * as mongoose from 'mongoose';
+import { Types } from 'mongoose';
 
 function isObjectIdValid(toCheck: string) {
-  return mongoose.Types.ObjectId.isValid(toCheck);
+  return Types.ObjectId.isValid(toCheck);
 }
 
 export { isObjectIdValid };
